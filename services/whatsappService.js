ff08916dcc6364122c6986575a4cf3b941c16377
LefikaSetuke <lefika.setuke@africacode.academy>
2025-09-05 10:45:24 +0200

    modified whatsapp api url

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -1,7 +1,7 @@
 // services/whatsappService.js
 const axios = require("axios");
 
-const WHATSAPP_API_URL = "https://graph.facebook.com/v18.0";
+const WHATSAPP_API_URL = "https://graph.facebook.com/v22.0";
 const PHONE_NUMBER_ID = process.env.WHATSAPP_PHONE_NUMBER_ID;
 const ACCESS_TOKEN = process.env.WHATSAPP_ACCESS_TOKEN;
 
